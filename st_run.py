ef8aeab01816d16426aa0e5aa2d865a5836bdb6d
Michael Riley <michaelriley811@gmail.com>
2024-03-22 08:53:56 -0500

    successfully deploy st app locally using previously ingested MongoDB data

---
+++ st_run.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import os
+
+from build_chain import chain_multimodal_rag as chain
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+
+st.title('🏥 FellowsGPT')
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"] # st.sidebar.text_input('OpenAI API Key', type='password')
+
+def generate_response(input_text):
+    llm = chain
+    st.info(llm.invoke(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the EV / NTM and NTM rev growth for MongoDB, Cloudflare, and Datadog?')
+    submitted = st.form_submit_button('Submit')
+    if not OPENAI_API_KEY.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and OPENAI_API_KEY.startswith('sk-'):
+        generate_response(text)
